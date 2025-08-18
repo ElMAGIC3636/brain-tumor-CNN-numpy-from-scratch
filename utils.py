2231b4bd2e9eba62742ec571b03d7f1091922e8c
ElMAGIC3636 <117826911+ElMAGIC3636@users.noreply.github.com>
2025-08-18 20:30:36 +0300

    utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np 
 
-# Relu Activation
+# in this case i used the categorical_crossEntropy for multiclass classification 
+
 def cross_entropy(pred, target):
     m = target.shape[0]
     log_pred = -np.log(np.clip(pred, 1e-12, 1.0))
